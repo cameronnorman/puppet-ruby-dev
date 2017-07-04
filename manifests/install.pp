e7cf8cb4f8114088ca142442e04f79ecb36f8058
Cameron Norman <cameronrossnorman@gmail.com>
2017-07-04 13:13:39 +0200

    made small changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -25,22 +25,28 @@ class install {
     ensure => installed,
   }
 
+  file { "/home/${username}/.ssh":
+    ensure => 'directory',
+    owner => "${username}",
+    group => "${username}"
+  }
+
   exec { 'generate ssh key':
-    command     => "ssh-keygen -t rsa -f /home/${username}/.ssh -q -N '' && 0",
-    #cwd         => '/home/cam',
+    command     => "ssh-keygen -t rsa -f /home/${username}/.ssh/id_rsa -q -N ''",
     user        => '${username}',
     creates     => '/home/${username}/.ssh/id_rsa',
     path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    returns     => [1]
+    #cwd         => '/home/cam',
+    #returns     => [1]
     #unless      => 'test param-that-would-be-true',
     #refreshonly => true,
   }
 
   exec { 'install ubuntu rvm':
-    command      => "sh /home/${username}/puppet-ruby-dev/modules/rvm_ubuntu.sh",
+    command      => "bash /home/${username}/puppet-ruby-dev/modules/rvm_ubuntu.sh",
     user        => '${username}',
     path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    returns     => [1]
+    #returns     => [1]
     #cwd         => '.',
     #creates     => '/file/created',
     #unless      => 'test param-that-would-be-true',
