27651cc7e9be00ab3e613653b198af44aececd28
Cameron Norman <cameronrossnorman@gmail.com>
2017-07-28 12:28:27 +0200

    postgres

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,20 +1,26 @@
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#!/bin/bash
+echo "Enter your username, followed by [ENTER]:"
+read username
+echo "Enter your email, followed by [ENTER]:"
+read email
 
-curl -sSL https://get.rvm.io | bash -s stable
+# sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
-source ~/.rvm/scripts/rvm
+# curl -sSL https://get.rvm.io | bash -s stable
 
-rvm install 2.4.0
+# source ~/.rvm/scripts/rvm
 
-rvm use 2.4.0 --default
+# rvm install 2.4.0
 
-gem install bundler
+# rvm use 2.4.0 --default
 
-git config --global color.ui true
+# gem install bundler
 
-ssh-keygen -t rsa -b 4096 -C "leonpaps@gmail.com"
+# ssh-keygen -t rsa -b 4096 -C $email
 
-git config --global user.name "Leon"
+# git config --global color.ui true
 
-git config --global user.email "leonpaps@gmail.com"
+# git config --global user.name $username
+
+# git config --global user.email $email
